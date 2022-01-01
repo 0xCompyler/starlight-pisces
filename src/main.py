d5c89503871281296e2d299d502fd5db7a8a5f3b
radioactive11 <roy.arijit2001@gmail.com>
2022-01-02 04:03:23 +0530

    :zap: `Optimized` Redis caching in search

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 import psycopg2
+import redis
 
 import logging
 from configparser import ConfigParser
 
+from scripts import search
 
+MODE = "development"
 
 app = FastAPI()
 origins = ["*"]
@@ -21,7 +24,6 @@ app.add_middleware(
 )
 
 logging.basicConfig(level=logging.INFO)
-logging.log(msg="Connecting to database", level=logging.INFO)
 
 
 def db_config(filename, section):
@@ -38,13 +40,37 @@ def db_config(filename, section):
 
     return creds
 
-logging.log(msg="Connecting to database", level=logging.INFO)
-
-creds = db_config('./database.ini', 'development')
-conn = psycopg2.connect(
+try:
+    logging.log(msg="Connecting to PG database", level=logging.INFO)
+    creds = db_config('./database.ini', 'development')
+    conn = psycopg2.connect(
         host=creds['host'],
         database=creds['database'],
         user=creds['user'],
         password=creds['password']
     )
+    logging.log(msg=f"Connection to {MODE} PG database established", level=logging.INFO)
+
+except Exception as e:
+    logging.critical("Error Connecting to PG database")
+
+
+try:
+    logging.log(msg="Connecting to Redis database", level=logging.INFO)
+    redis_client = redis.Redis(host='localhost', port=6379, db=1)
+    logging.log(msg=f"Connection to {MODE} redis established", level=logging.INFO)
+
+except Exception as e:
+    logging.critical(f"Error Connecting to Redis database: {e}")
+
+class SearchResource(BaseModel):
+    query: str
+
+@app.post('/search')
+def _search(request_body: SearchResource):
+    logging.log(msg=f"Search hit with {request_body.query=}", level=logging.DEBUG)
+    results = search.search_stocks(redis_client, conn, request_body.query)
+
+    return results
+
 
--- src/scripts/search.py
+++ src/scripts/search.py
@@ -1,35 +1,46 @@
 import psycopg2
 from psycopg2.extensions import connection
 
+import json
+import datetime
+import redis
+from redis.client import Redis
 
-def search_stocks(conn: connection, query: str):
-    cur = conn.cursor()
+def search_stocks(redis_client: Redis, conn: connection, query: str):
+    cached_data = redis_client.get(query)
 
-    try:
-        cur.execute(
-            """
-            SELECT st.isin_number, st.symbol, st.name FROM stocks st
-            INNER JOIN search sch 
-            ON st.isin_number = sch.isin_number
-            ORDER BY SIMILARITY(search_col, %s) DESC
-            LIMIT 10;
-            """,
-        (query, ))
+    if cached_data is not None:
+        json_data = json.loads(cached_data)
+        return json_data
 
-    except Exception as e:
-        return {'error': e}
+    else:
+        cur = conn.cursor()
+        try:
+            cur.execute(
+                """
+                SELECT st.isin_number, st.symbol, st.name FROM stocks st
+                INNER JOIN search sch 
+                ON st.isin_number = sch.isin_number
+                ORDER BY SIMILARITY(search_col, %s) DESC
+                LIMIT 10;
+                """,
+            (query, ))
 
-    query_results = cur.fetchall()
-    results = []
+        except Exception as e:
+            return {'error': e}
 
-    for item in query_results:
-        temp_dict = {
-            "isin_number": item[0],
-            "symbol": item[1],
-            "name": item[2]
-        }
+        query_results = cur.fetchall()
+        results = []
 
-        results.append(temp_dict)
-    
-    return results
+        for item in query_results:
+            temp_dict = {
+                "isin_number": item[0],
+                "symbol": item[1],
+                "name": item[2]
+            }
+
+            results.append(temp_dict)
+
+        redis_client.setex(query, datetime.timedelta(hours=24), json.dumps(results))
+        return results
 
